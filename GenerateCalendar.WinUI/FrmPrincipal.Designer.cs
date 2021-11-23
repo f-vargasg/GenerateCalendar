e0ed997cfb1f36553d8df56cf39c855dfbe4b2b5
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2021-11-22 21:05:53 -0600

    Agregar archivos de proyecto.

---
+++ GenerateCalendar.WinUI/FrmPrincipal.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace GenerateCalendar.WinUI
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tlsStrSalir = new System.Windows.Forms.ToolStripButton();
+            this.tlsStrDo = new System.Windows.Forms.ToolStripButton();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgrCalendar = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrCalendar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 87);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Output";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(-1, 87);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(858, 189);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtYear);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtMes);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(-1, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(710, 59);
+            this.panel1.TabIndex = 6;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tlsStrSalir,
+            this.tlsStrDo});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(855, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tlsStrSalir
+            // 
+            this.tlsStrSalir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tlsStrSalir.Image = ((System.Drawing.Image)(resources.GetObject("tlsStrSalir.Image")));
+            this.tlsStrSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlsStrSalir.Name = "tlsStrSalir";
+            this.tlsStrSalir.Size = new System.Drawing.Size(23, 22);
+            this.tlsStrSalir.Text = "toolStripButton1";
+            this.tlsStrSalir.ToolTipText = "Exit";
+            this.tlsStrSalir.Click += new System.EventHandler(this.tlsStrSalir_Click);
+            // 
+            // tlsStrDo
+            // 
+            this.tlsStrDo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tlsStrDo.Image = ((System.Drawing.Image)(resources.GetObject("tlsStrDo.Image")));
+            this.tlsStrDo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlsStrDo.Name = "tlsStrDo";
+            this.tlsStrDo.Size = new System.Drawing.Size(23, 22);
+            this.tlsStrDo.Text = "toolStripButton2";
+            this.tlsStrDo.ToolTipText = "Do Process";
+            this.tlsStrDo.Click += new System.EventHandler(this.tlsStrDo_Click);
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(180, 21);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(51, 22);
+            this.txtYear.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(138, 24);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Año";
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(65, 21);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(34, 22);
+            this.txtMes.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 24);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mes:";
+            // 
+            // dgrCalendar
+            // 
+            this.dgrCalendar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgrCalendar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgrCalendar.Location = new System.Drawing.Point(0, 282);
+            this.dgrCalendar.Name = "dgrCalendar";
+            this.dgrCalendar.Size = new System.Drawing.Size(857, 216);
+            this.dgrCalendar.TabIndex = 8;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 498);
+            this.Controls.Add(this.dgrCalendar);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmPrincipal";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgrCalendar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tlsStrSalir;
+        private System.Windows.Forms.ToolStripButton tlsStrDo;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgrCalendar;
+    }
+}
+
---
+++ GenerateCalendar.WinUI/FrmPrincipal.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenerateCalendar.WinUI
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            InitMyComponents();
+        }
+
+
+
+        private void InitMyComponents()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = ConfigurationManager.AppSettings["formCaption"];
+            DateTime scrap = DateTime.Now;
+            txtMes.Text = scrap.Month.ToString();
+            txtYear.Text = scrap.Year.ToString();
+        }
+
+
+        // Function that returns the index of the
+        // day for date DD/MM/YYYY 
+        public static int DayOfWeek(int day, int month, int year)
+        {
+
+            int res = 0;
+            /*
+            int[] t = { 0, 3, 2, 5, 0, 3, 5, 
+                      1, 4, 6, 2, 4 };
+            
+            year -= ((month < 3) ? 1 : 0);
+
+            res = (year + year / 4 - year / 100 + year / 400
+                             + t[month - 1] + day) % 7;
+            */
+            DateTime scrap = new DateTime(year, month + 1, day + 1);
+            res = (int)(scrap.DayOfWeek);
+            return res;
+        }
+
+        // Function that returns the name of the 
+        // month for the given month Number 
+        // January - 0, February - 1 and so on 
+        public static string GetMonthName(int monthNumber)
+        {
+            string month = string.Empty;
+
+            switch (monthNumber)
+            {
+                case 0:
+                    month = "January";
+                    break;
+                case 1:
+                    month = "February";
+                    break;
+                case 2:
+                    month = "March";
+                    break;
+                case 3:
+                    month = "April";
+                    break;
+                case 4:
+                    month = "May";
+                    break;
+                case 5:
+                    month = "June";
+                    break;
+                case 6:
+                    month = "July";
+                    break;
+                case 7:
+                    month = "August";
+                    break;
+                case 8:
+                    month = "September";
+                    break;
+                case 9:
+                    month = "October";
+                    break;
+                case 10:
+                    month = "November";
+                    break;
+                case 11:
+                    month = "December";
+                    break;
+            }
+            return month;
+        }
+
+        // Function to return the number of days 
+        // in a month 
+        public static int NumberOfDays(int monthNumber, int year)
+        {
+            // January 
+            if (monthNumber == 0)
+                return (31);
+
+            // February 
+            if (monthNumber == 1)
+            {
+                // If the year is leap then Feb 
+                // has 29 days 
+                if (year % 400 == 0
+                    || (year % 4 == 0
+                        && year % 100 != 0))
+                    return (29);
+                else
+                    return (28);
+            }
+
+            // March 
+            if (monthNumber == 2)
+                return (31);
+
+            // April 
+            if (monthNumber == 3)
+                return (30);
+
+            // May 
+            if (monthNumber == 4)
+                return (31);
+
+            // June 
+            if (monthNumber == 5)
+                return (30);
+
+            // July 
+            if (monthNumber == 6)
+                return (31);
+
+            // August 
+            if (monthNumber == 7)
+                return (31);
+
+            // September 
+            if (monthNumber == 8)
+                return (30);
+
+            // October 
+            if (monthNumber == 9)
+                return (31);
+
+            // November 
+            if (monthNumber == 10)
+                return (30);
+
+            // December 
+            if (monthNumber == 11)
+                return (31);
+
+            return -1;
+        }
+
+        DataTable BuildCalendar(int month, int year)
+        {
+            DataTable dtRes = null;
+            DataRow dtRow = null;
+            string res = string.Empty;
+            int days;
+
+            try
+            {
+                dtRes = new DataTable();
+                
+                dtRes.Columns.Add("Sun");
+                dtRes.Columns.Add("Mon");
+                dtRes.Columns.Add("Tue");
+                dtRes.Columns.Add("Wed");
+                dtRes.Columns.Add("Thu");
+                dtRes.Columns.Add("Fri");
+                dtRes.Columns.Add("Sat");
+                // Index of the day from 0 to 6 
+                int current = DayOfWeek(0, month, year);
+
+                // i for Iterate through months 
+                // j for Iterate through days 
+                // of the month - i 
+
+                days = NumberOfDays(month, year);
+                string nomCol = string.Empty;
+                int k;
+                string[] nomColums = {"Sun", "Mon", "Tue", "Wed", "Thu",
+                                      "Fri", "Sat"};
+                DataRow dr = dtRes.NewRow();
+                for (k = 0; k < current; k++)
+                {
+                    nomCol = nomColums[k];
+                    dr[nomCol] = null;      // res += "     ";
+                }
+                    
+                    
+
+                for (int j = 1; j <= days; j++)
+                {
+                    // printf("%5d", j);
+                    res += string.Format("{0,5:##}", j);
+                    nomCol = nomColums[(j - 1) % 6];
+                    dr[nomCol] = j;
+
+                    if ((++k) > 6)
+                    {
+                        k = 0;
+                        dtRes.Rows.Add(dr);
+                        dr = dtRes.NewRow();
+                        res += Environment.NewLine;
+                        dr = dtRes.NewRow();
+                    }
+                }
+
+                if (k == 0) // new line
+                    res += Environment.NewLine;
+                return dtRes;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+
+
+        }
+
+        // Function to print the calendar of 
+        // the given year 
+        string PrintCalendar(int month, int year)
+        {
+            string res = string.Empty;
+            res = string.Format("     Calendar - {0}"+ Environment.NewLine + Environment.NewLine , year);
+            int days;
+
+            // Index of the day from 0 to 6 
+            int current = DayOfWeek(0, month, year);
+
+            // i for Iterate through months 
+            // j for Iterate through days 
+            // of the month - i 
+
+            days = NumberOfDays(month, year);
+
+            // Print the current month name 
+            res += string.Format("{0} ------------{1}-------------{2}",
+                                 Environment.NewLine,  GetMonthName(month), 
+                                 Environment.NewLine);
+
+            // Print the columns 
+            res += string.Format(" Sun   Mon  Tue  Wed  Thu  Fri  Sat{0}", 
+                                 Environment.NewLine);
+
+            // Print appropriate spaces 
+            int k;
+            for (k = 0; k < current; k++)
+                res += "     ";
+
+            for (int j = 1; j <= days; j++)
+            {
+                // printf("%5d", j);
+                res += string.Format("{0,5:##}", j);
+
+                if ((++k) > 6)
+                {
+                    k = 0;
+                    res += Environment.NewLine;
+                }
+            }
+
+            if (k == 0)
+                res += Environment.NewLine;
+
+            current = k;
+
+
+            return res;
+        }
+
+        private void tlsStrSalir_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void tlsStrDo_Click(object sender, EventArgs e)
+        {
+            int mes = Convert.ToInt32(txtMes.Text) - 1;
+            int year = Convert.ToInt32(txtYear.Text);
+            txtOutput.Text = PrintCalendar(mes, year);
+            DataTable dt = BuildCalendar(mes, year);
+        }
+    }
+
+
+
+
+}
